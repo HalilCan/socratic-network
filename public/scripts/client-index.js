f776624c3d67175ff508140fe47aeb755286af20
HalilCan <halilcanmemoglu@gmail.com>
2018-08-23 21:53:15 +0300

    Post body automated formatting

--- public/scripts/client-index.js
+++ public/scripts/client-index.js
@@ -15,5 +15,14 @@ socket.on('post by index', function (data) {
 });
 
 function formatPost (postObject) {
-    return "<div class = 'post' id = `post" + postObject.index +"`> <div class = 'post-title'>" + postObject.title + "</div> <div class = 'post-subtitle'>" + postObject.subtitle + "</div> <div class = 'post-subject'>" + postObject.subjects.join(' ') + "</div> <div class = 'post-body'>" + postObject.body + "</div> <div class = 'post-labels'>" + postObject.labels.toString() + "</div> <div class = 'post-separator'></div> </div>";
+    return "<div class = 'post' id = `post" + postObject.index +"`> <div class = 'post-title'>" + postObject.title + "</div> <div class = 'post-subtitle'>" + postObject.subtitle + "</div> <div class = 'post-subject'>" + postObject.subjects.join(' ') + "</div> <div class = 'post-body'>" + getformattedBody(postObject.body) + "</div> <div class = 'post-labels'>" + postObject.labels.toString() + "</div> <div class = 'post-separator'></div> </div>";
+}
+
+function getformattedBody (body) {
+    let bodyArray = body.split('\n');
+    let formattedBody;
+    for (let paragraph of bodyArray) {
+        formattedBody += "<div class = 'post-paragraph'>" + paragraph + "</div>"
+    }
+    return formattedBody;
 }
