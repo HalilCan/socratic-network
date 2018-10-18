c0c12ad55d7f127db2b53aa88079df6f78b9c624
HalilCan <halilcanmemoglu@gmail.com>
2018-10-17 23:51:47 -0400

    Socratic 0.3.1
    Done:
    html href path fixes
    
    In Progress:
    Build admin dashboard
    Test syncUpward
    
    Planned:
    Submission bodies should have markdown support.
    Checkbox labels
    Filters and sort

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -2,5 +2,39 @@ let adminPw = "testPw";
 let isAdminPw = (q) => {
     return q === adminPw;
 };
+let generateAdminDashboard = () => {
+    return (`<!DOCTYPE HTML>
+
+    <html lang="en">
+        <head>
+        <meta charset="UTF-8">
+        <title>Admin Dashboard</title>
+    <link href="https://fonts.googleapis.com/css?family=Lora|Montserrat" rel="stylesheet">
+        <link rel="stylesheet" href="../styles/fundamental.css">
+
+        <script type="text/javascript" src="../scripts/socket.io-client-master/dist/socket.io.js"></script>
+        <script type="text/javascript" src="../scripts/client-admin.js"></script>
+        <script type="text/javascript" src="../scripts/element-formatter.js"></script>
+        <script type="text/javascript" src="../scripts/server-requests.js"></script>
+        <script type="text/javascript" src="../scripts/utility.js"></script>
+        </head>
+
+        <body>
+
+        <div class="page-navbar">
+        <ul class="topnav">
+        <li class="left" id="navbar-left"><a id="hamburger" href="javascript:">☰</a></li>
+    <li><a onclick="writeMode()" href="javascript:">Write</a></li>
+    <li class="right" id="navbar-right"><a onclick="requestArchiveBackup()" href="javascript:">Backup</a></li>
+    </ul>
+    </div>
+
+    <div id="label-list"></div>
+        <div id="real-estate"></div>
+
+        </body>`
+);
+};
 
 exports.isAdminPw = isAdminPw;
+exports.generateAdminDashboard = generateAdminDashboard;
